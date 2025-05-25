f7b3be6cc6fe0b0629aa800e6f3c642387035279
Douglas Marques <douglas.cmarques2@gmail.com>
2025-05-25 14:56:59 -0300

    config eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        jest: true,
+        console: "readonly", // Adicionando o console como global
+        __dirname: "readonly",
+      },
+    },
+  },
+];
